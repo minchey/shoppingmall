f655b66988101923f93b7c313447ea7d774e1b3e
강민제 <98521434+minchey@users.noreply.github.com>
2025-06-28 17:26:13 +0900

    appConfig 코드분석

--- src/main/java/com/example/loginproject/config/AppConfig.java
+++ src/main/java/com/example/loginproject/config/AppConfig.java
@@ -1,15 +1,15 @@
 package com.example.loginproject.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.context.annotation.Bean; //bean 어노테이션 사용
+import org.springframework.context.annotation.Configuration; // configuration 어노테이션 사용
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder; //실제로 사용하는 암호화 클래스
+import org.springframework.security.crypto.password.PasswordEncoder; //인터페이스 타입으로 반환 -> 스프링 di용
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder() { //코드 변경의 유연함을 위해 만든 클래스. 나중에 return 만 변경해주면 됩
         return new BCryptPasswordEncoder(); //spring 기본 제공 암호화 코드. 단반향으로 복호화 불가능
     }
 }
