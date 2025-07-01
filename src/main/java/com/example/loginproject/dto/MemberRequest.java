6d392922ba3a383c51cb1aa86ce39d0fee34aa4d
강민제 <98521434+minchey@users.noreply.github.com>
2025-07-01 22:42:28 +0900

    MemberRequest 코드분석

--- src/main/java/com/example/loginproject/dto/MemberRequest.java
+++ src/main/java/com/example/loginproject/dto/MemberRequest.java
@@ -1,13 +1,15 @@
-package com.example.loginproject.dto;
+package com.example.loginproject.dto; //폼에서 입력한 데이터 전달 하기 위한 객체
 
-import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank; //필드가 null이거나 빈문자열이면 유효하지 않다는 유효성 검사 어노테이션
+import lombok.Getter; //lombok을 통해 getter 자동생성
+import lombok.Setter; //lombok을 통해 setter 자동생성
 
 @Getter
 @Setter
-public class MemberRequest {
+public class MemberRequest { //회원가입 폼 데이터 담는 용도 @ModerAttribute로 이 DTO 자동으로 바인딩
 
+
+    //빈칸인지 확인 하는 코드 빈칸일시 에러메시지 출력
     @NotBlank(message = "아이디는 필수입니다.")
     private String username;
 
