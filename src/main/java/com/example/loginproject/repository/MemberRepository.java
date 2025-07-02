6febea70edf5e2ee705150a8c015bb78b2d37e39
강민제 <98521434+minchey@users.noreply.github.com>
2025-07-03 00:20:31 +0900

    postrepository 코드분석 마무리

--- src/main/java/com/example/loginproject/repository/MemberRepository.java
+++ src/main/java/com/example/loginproject/repository/MemberRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository; //스프링이 제
 
 import java.util.Optional; //값이 있을수도 없을수도 있다는 명확하게 표현하는 자료형
 
-public interface MemberRepository extends JpaRepository<Member, Long> { //JPA가 자동으로 구현체를 만들어주는 인터페이 member - 테이블명, long - id의 자료형
+public interface MemberRepository extends JpaRepository<Member, Long> { //JPA가 자동으로 구현체를 만들어주는 인터페이스 member - 테이블명, long - id의 자료형
     Optional<Member> findByUsername(String username); //커스텀 매서드 정의 (자동쿼리생성) username으로 회원찾기
     //optional인 이유는 회원이 있으면 값을 주고 없으면 빈값을 줌
 }
--- src/main/java/com/example/loginproject/repository/PostRepository.java
+++ src/main/java/com/example/loginproject/repository/PostRepository.java
@@ -1,9 +1,9 @@
 package com.example.loginproject.repository; //리포지토리 위치 선언
 
 import com.example.loginproject.domain.Post; //엔티티 사용
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository; //JPA가 기본 CRUD 기능을 사용할 수 있게 해줌
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-
+//JpaRepository<Post, Long>을 상속 받아서 Post테이블을 조작할 수 있는 기본 기능 부여 받음
 }
 
