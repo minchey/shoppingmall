bec2e74e6dc6c8de0a4f29a00c78d198a3638b17
강민제 <98521434+minchey@users.noreply.github.com>
2025-07-02 23:56:52 +0900

    postrepository 코드분석

--- src/main/java/com/example/loginproject/repository/PostRepository.java
+++ src/main/java/com/example/loginproject/repository/PostRepository.java
@@ -1,6 +1,6 @@
-package com.example.loginproject.repository;
+package com.example.loginproject.repository; //리포지토리 위치 선언
 
-import com.example.loginproject.domain.Post;
+import com.example.loginproject.domain.Post; //엔티티 사용
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
